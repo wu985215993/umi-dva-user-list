7618330f981fe3334d99a17b2911aeaebb629652
StickerWu <985215993@qq.com>
2021-06-17 01:11:49 +0800

    TS改写model

--- src/pages/users/model.ts
+++ src/pages/users/model.ts
@@ -1,47 +1,56 @@
-export default {
-    namespace: 'users',
-    state:{
-    },
-    effects:{
-
-    },
-    reducers:{
-        save(state,action){
-            const data = [
-                {
-                  key: '1',
-                  name: 'John Brown',
-                  age: 32,
-                  address: 'New York No. 1 Lake Park',
-                  tags: ['nice', 'developer'],
-                },
-                {
-                  key: '2',
-                  name: 'Jim Green',
-                  age: 42,
-                  address: 'London No. 1 Lake Park',
-                  tags: ['loser'],
-                },
-                {
-                  key: '3',
-                  name: 'Joe Black',
-                  age: 32,
-                  address: 'Sidney No. 1 Lake Park',
-                  tags: ['cool', 'teacher'],
-                },
-              ];
-            return data
-        }
-    },
-    subscriptions:{
-        setup({dispatch,history}) {
-            return history.listen(({pathname}) =>{
-                if(pathname === '/users'){
-                    dispatch({
-                        type:"save"
-                    })
-                }
-            })
-        }
-    }
+import { Effect, Subscription } from 'dva';
+import { Reducer } from 'react';
+interface UserModelType {
+  namespace: 'users';
+  state: {};
+  reducers: { getList: Reducer };
+  effects: {};
+  subscriptions: {
+    setup: Subscription;
+  };
 }
+const UserModel: UserModelType = {
+  namespace: 'users',
+  state: {},
+  reducers: {
+    getList(state, action) {
+      const data = [
+        {
+          key: '1',
+          name: 'John Brown',
+          age: 32,
+          address: 'New York No. 1 Lake Park',
+          tags: ['nice', 'developer'],
+        },
+        {
+          key: '2',
+          name: 'Jim Green',
+          age: 42,
+          address: 'London No. 1 Lake Park',
+          tags: ['loser'],
+        },
+        {
+          key: '3',
+          name: 'Joe Black',
+          age: 32,
+          address: 'Sidney No. 1 Lake Park',
+          tags: ['cool', 'teacher'],
+        },
+      ];
+      return data;
+    },
+  },
+  effects: {},
+  subscriptions: {
+    setup({ dispatch, history }) {
+      return history.listen(({ pathname }) => {
+        if (pathname === '/users') {
+          dispatch({
+            type: 'getList',
+          });
+        }
+      });
+    },
+  },
+};
+export default UserModel;
